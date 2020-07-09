caf2c7f7c0eedb54853c71322d08b8689121af27
F Ishikawa <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2020-07-09 15:19:35 +0900

    clean messy codes

--- include/exact_diagonalization.py
+++ include/exact_diagonalization.py
@@ -12,16 +12,23 @@ class ExactDiagonalization:
 
     def __call__(self,density_matrix,t):
         N = self.N
-        propagator_left = np.zeros((np.power(2,N),np.power(2,N)),dtype=np.complex128)
-        propagator_right = np.zeros((np.power(2,N),np.power(2,N)),dtype=np.complex128)
+        propagator_left = \
+                np.zeros((np.power(2,N),np.power(2,N)),dtype=np.complex128)
+        propagator_right = \
+                np.zeros((np.power(2,N),np.power(2,N)),dtype=np.complex128)
         D_temp = np.zeros(np.power(2,N), dtype=np.complex128)
         for i in range(self.size):
             D_temp[i] = np.exp(-1.0j * t * (self.D[i] - self.D_min))
         D_temp = np.diag(D_temp)
         D_temp_inv = np.conjugate(D_temp)
-        propagator_left = np.matmul(np.matmul(self.P, D_temp) , np.conjugate(self.P.transpose()))
-        propagator_right = np.matmul(np.matmul(self.P, D_temp_inv) , np.conjugate(self.P.transpose()))
-        rho_temp = np.matmul(np.matmul(propagator_left, density_matrix.density_matrix), propagator_right) 
+        propagator_left = \
+                np.matmul(np.matmul(self.P, D_temp), 
+                          np.conjugate(self.P.transpose()))
+        propagator_right = \
+                np.matmul(np.matmul(self.P, D_temp_inv), 
+                          np.conjugate(self.P.transpose()))
+        rho_temp = np.matmul(np.matmul(propagator_left, 
+                             density_matrix.density_matrix), propagator_right) 
         density_matrix_temp = density_matrix.copy()
         density_matrix_temp.density_matrix = rho_temp / np.trace(rho_temp).real
         return density_matrix_temp
